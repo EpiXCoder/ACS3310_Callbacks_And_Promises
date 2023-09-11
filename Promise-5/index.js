1cfb6108e553972103eac5e5262ffbcdae412595
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-09-11 14:28:13 -0400

    Resolved Promist-5

--- Promise-5/index.js
+++ Promise-5/index.js
@@ -1,6 +1,26 @@
 function whatToEat(timeOfDay) {
   // Create a new promise and return it here
-  
+  let meal = '';
+  switch(timeOfDay.toLowerCase()) {
+    case "breakfast":
+      meal = "Eggs on toast";
+      break;
+    case "lunch":
+      meal = "Ramen";
+      break;
+    case "dinner":
+      meal = "Dosa";
+      break;
+  }
+  return new Promise((resolve, reject) => {
+    if (meal != '') { 
+        resolve(meal);
+      } else {
+        reject("I'm not hungry right now");
+      };
+    })
+    .then(food => console.log(food))
+    .catch(err => console.log(err))
 }
 
 // whatToEat should return a promise. 
@@ -11,7 +31,9 @@ function whatToEat(timeOfDay) {
 
 // Handle the Promise returned from whatToEat here: 
 
+whatToEat('breakfast')
 whatToEat('lunch')
+whatToEat('dinner')
 
 // Handle this Promise: 
 whatToEat('elevens')
