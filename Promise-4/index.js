4d13a82cfe248c61fa24bf9190825f971e1d6c88
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-09-11 14:06:48 -0400

    Resolved Promist-4

--- Promise-4/index.js
+++ Promise-4/index.js
@@ -1,5 +1,5 @@
 const food = new Promise((resolve, reject) => {
-  
+  resolve("Poké")
 })
 
 
@@ -12,3 +12,4 @@ const food = new Promise((resolve, reject) => {
 
 // **Challenge 2** Resolve the promise `food` with the `.then()` syntax 
 // and print your favorite food to the console. 
+food.then(str => console.log(str))
